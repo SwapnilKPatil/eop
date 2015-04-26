098fd2e2352cb830f197c3c59ef146f75e8f29b2
SwapnilKPatil <swapnil12984@gmail.com>
2015-04-26 14:35:53 -0400

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,13 +1,13 @@
 public class Node {
-	public int val;
-	public Node left=null;
-	public Node right=null;
-	public Node parent=null;
+    public int val;
+    public Node left=null;
+    public Node right=null;
+    public Node parent=null;
 	
-	public Node(int val){
-		this.left = left;
-		this.right = right;
-		this.val = val;
-		this.parent = parent;
-	}
+    public Node(int val){
+	this.left = left;
+	this.right = right;
+	this.val = val;
+	this.parent = parent;
+    }
 }
