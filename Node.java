2743d703cee90d8da009a265f580e5a60f72261e
SwapnilKPatil <swapnil12984@gmail.com>
2015-04-26 14:27:50 -0400

    9.1

---
+++ Node.java
@@ -0,0 +1,13 @@
+public class Node {
+	public int val;
+	public Node left=null;
+	public Node right=null;
+	public Node parent=null;
+	
+	public Node(int val){
+		this.left = left;
+		this.right = right;
+		this.val = val;
+		this.parent = parent;
+	}
+}
